c8cc252b5c036dc1cc18262611fec1fef9bec552
raguileoam <raguileoam@gmail.com>
2023-03-11 10:20:16 -0300

    Fix ticket module

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
 import { Ticket } from './entities/ticket.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [Ticket],
+  imports: [TypeOrmModule.forFeature([Ticket])],
   controllers: [TicketsController],
   providers: [TicketsService],
-  exports: [TicketsService, TicketsController],
+  exports: [TicketsService],
 })
 export class TicketsModule {}
